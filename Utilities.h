c5c3508e352e60c851e9ee06e67608462de463f1
strtPath <75707906+strtPath@users.noreply.github.com>
2026-08-09 23:33:58 -0400

    Add Wio Tracker L1 LED handlers

--- Utilities.h
+++ Utilities.h
@@ -356,6 +356,13 @@ uint8_t boot_vector = 0x00;
 		void led_tx_off() { digitalWrite(pin_led_tx, LOW); }
 		void led_id_on()  { }
 		void led_id_off() { }
+	#elif BOARD_MODEL == BOARD_WIO_TRACKER_L1
+  		void led_rx_on()  { digitalWrite(pin_led_rx, HIGH); }
+  		void led_rx_off() { digitalWrite(pin_led_rx, LOW); }
+  		void led_tx_on()  { digitalWrite(pin_led_tx, HIGH); }
+  		void led_tx_off() { digitalWrite(pin_led_tx, LOW); }
+  		void led_id_on()  { }
+  		void led_id_off() { }
 	#elif BOARD_MODEL == BOARD_HELTEC_T114
     // Heltec T114 pulls pins LOW to turn on
     void led_rx_on()  { digitalWrite(pin_led_rx, LOW); }
